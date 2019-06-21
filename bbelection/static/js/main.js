e4bd8c3be71575629428b2e5e802cf788df72f39
TIMUR <1tkurbanov@gmail.com>
2019-06-21 09:52:23 -0400

    done

--- bbelection/static/js/main.js
+++ bbelection/static/js/main.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+axios.get('https://bb-election-api.herokuapp.com/')
+  .then((response) => {
+    let data = response.data.candidates;
+    let candidateList = document.querySelector('#candidates');
+    data.forEach(function (candidate) {
+      let candidateInfo = document.createElement('li');
+      candidateInfo.innerText = candidate.name + '\n' + 'votes:' + candidate.votes
+      candidateList.appendChild(candidateInfo)
+    })
+  });
 });
